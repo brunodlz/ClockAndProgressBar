bb4eba5c84cbeca2f5ef8ec7cde2e365cab7a281
Bruno da Luz <brunolabx@gmail.com>
2014-10-20 21:29:26 -0200

    Initial Commit

---
+++ ClockAndProgressBarTests/ClockAndProgressBarTests.m
@@ -0,0 +1,40 @@
+//
+//  ClockAndProgressBarTests.m
+//  ClockAndProgressBarTests
+//
+//  Created by Bruno on 10/20/14.
+//  Copyright (c) 2014 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ClockAndProgressBarTests : XCTestCase
+
+@end
+
+@implementation ClockAndProgressBarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
